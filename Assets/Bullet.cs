d3e2698f230f124c7ef949acb1cddb3e8f4751f1
gameegg <whddnjs2860@naver.com>
2015-10-29 02:32:01 +0900

    프로토타이핑1

---
+++ Assets/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public const float speed = 10;
+	public const float damage = 1;
+
+	public float angle;
+
+
+	bool destroyed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dt = Time.deltaTime;
+
+
+		transform.localRotation = Quaternion.Euler (Vector3.up * angle);
+		transform.localPosition += (transform.localRotation * Vector3.right * dt * speed);
+	}
+
+	void LateUpdate(){
+
+	}
+
+	void OnTriggerEnter(Collider cd){
+		Debug.Log (cd.tag);
+		if (cd.tag == "SpaceShip" && !destroyed) {
+			cd.GetComponent<SpaceShip> ().Damage (damage);
+			destroyed = true;
+			Destroy (gameObject);
+		} else if (cd.tag == "Bullet" && !destroyed) {
+			destroyed = true;
+			Destroy (gameObject);
+		}
+
+	}
+}
---
+++ Assets/SpaceShipHpBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipHpBar : MonoBehaviour {
+
+	public SpaceShip stat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RectTransform rt = GetComponent<RectTransform> ();
+		rt.localScale = Vector3.one - Vector3.right * (1-stat.hp/SpaceShip.maxHp);
+	}
+}
