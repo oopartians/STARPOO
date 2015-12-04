79ad8fc5b6fb1b5f8ccf100689b91076df5d76c9
Gyeongmo Lim <oscarkrkr@gmail.com>
2015-12-04 10:35:00 +0900

    remove OnTriggerEnter2D Trigger and Radar Circle Collider

--- Assets/Radar.cs
+++ Assets/Radar.cs
@@ -2,44 +2,38 @@
 using System.Collections.Generic;
 
 public class Radar : MonoBehaviour {
+    public SpaceShipHandler spaceShip;
+
+    public float radarRadius;
+    private CircleCollider2D circleCollider2D;
 
     public List<SpaceShipHandler> spaceShips_alliance;
+    public SpaceShipHandler[] spaceShips_alliancea;
     public List<SpaceShipHandler> spaceShips_enemy;
-    public List<Bullet> bullets;
+    public Bullet[] bullets;
 
     // Use this for initialization
     void Start () {
         Debug.Log("Hello Radar");
 
+        // Update Radar Radius for develop.
+        circleCollider2D = transform.GetComponent<CircleCollider2D>();
+        circleCollider2D.radius = radarRadius;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-    void OnTriggerEnter2D(Collider2D cd)
-    {
-        // Q. destroy 를 체크해야하는가?
-        //if (destroyed)
-        //{
-        //    return;
-        //}
-
-        switch (cd.tag)
+        bullets = GameObject.FindObjectsOfType<Bullet>();
+        Debug.Log("[Radar] Current Bullets count : " + bullets.Length);
+        
+        SpaceShipHandler[] spaceShips = GameObject.FindObjectsOfType<SpaceShipHandler>();
+        spaceShips_alliancea = GameObject.FindObjectsOfType<SpaceShipHandler>();
+        foreach (SpaceShipHandler _spaceShip in spaceShips)
         {
-            case "Wall":
-                break;
-            case "Radar":
-                break;
-            case "Bullet":
-                Debug.Log("Bullet!!!");
-                
-                // Add Bullet Information in Radar
-                break;
-            case "SpaceShip":
-                // Add SpaceShip Information in Radar
-                break;
+            if (_spaceShip.fleet.name.Equals(spaceShip.fleet.name))
+                spaceShips_alliance.Add(_spaceShip);
+            else
+                spaceShips_enemy.Add(_spaceShip);
         }
     }
 }
