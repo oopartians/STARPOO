dbc84eca06ee9c2448403adc4100238f7d01f08d
Gyeongmo Lim <oscarkrkr@gmail.com>
2015-12-13 20:51:45 +0900

    add TeamColor which set look good color

--- Assets/Scripts/MakeTeam/TeamListPannel.cs
+++ Assets/Scripts/MakeTeam/TeamListPannel.cs
@@ -1,47 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class TeamListPannel : MonoBehaviour {
 	List<GameObject> teamPannels = new List<GameObject>();
-	List<Color> colors = new List<Color>();
+    TeamColor teamColor = new TeamColor();
+    private int defaultColorCount = 13;
 
 	// Use this for initialization
 	void Start () {
-		AddTeam();
+
+        //GameObject jsLoader = GameObject.Find("ScriptList");
+        teamColor.SetColorsList(defaultColorCount); // jsLoader.transform.childCount
+        //Debug.Log("[TeamListPannel] JS count : " + jsLoader.transform.childCount);
+
+        AddTeam();
 		AddTeam();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
-
 
 	public void AddTeam(){
 		GameObject pannel = (GameObject)Instantiate(Resources.Load("TeamPannel"));
 		pannel.transform.SetParent(transform);
 		pannel.transform.localScale = Vector3.one;
+        
+        pannel.GetComponent<Image> ().color = teamColor.DequeueTeamColor();
 
-		Color color = new Color (Random.Range (0.2f, 1), Random.Range (0.2f, 1), Random.Range (0.2f, 1),0.5f);
-
-		pannel.GetComponent<Image> ().color = color;
-
-		teamPannels.Add(pannel);
+        teamPannels.Add(pannel);
 	}
+
 	public void RemoveTeam(){
 		if(teamPannels.Count <= 2){
 			return;
 		}
 		foreach(GameObject pannel in teamPannels){
 			if(pannel.transform.childCount <= 0){
+                teamColor.EnQueueTeamColor(pannel.GetComponent<Image>().color);
 				teamPannels.Remove(pannel);
 				Destroy (pannel);
 				break;
 			}
 		}
 	}
-
 
 	public void Complete(){
 		foreach(GameObject pannel in teamPannels){
@@ -56,5 +62,4 @@ public class TeamListPannel : MonoBehaviour {
 		}
 		Match.CompleteMakeTeams ();
 	}
-
 }
