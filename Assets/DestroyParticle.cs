52b29d7df40b933adaea40e47e44c305b9c84bc5
gameegg <jongwon.baek@redduck.com>
2016-01-22 19:37:11 +0900

    destroy particle of ship and bullet

---
+++ Assets/DestroyParticle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour {
+	public GameObject particle;
+	// Use this for initialization
+	void OnDestroy(){
+		GameObject p = (GameObject)Instantiate(particle);
+		p.transform.position = transform.position;
+	}
+}
---
+++ Assets/ParticleAutoDestruct.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleAutoDestruct : MonoBehaviour {
+
+	public  ParticleSystem thisParticleSystem;
+
+	// Update is called once per frame
+	void Update() {
+		if (thisParticleSystem.isPlaying)
+			return;
+
+		Destroy (gameObject);
+	}
+}
