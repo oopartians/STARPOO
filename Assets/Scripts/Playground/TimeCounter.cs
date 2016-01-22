0d6cf4b26cb0505780a014a7cc698a6ad8d84403
gameegg <jongwon.baek@redduck.com>
2016-01-22 16:41:02 +0900

    if timer is lower than 3sec, timer be red

--- Assets/Scripts/Playground/TimeCounter.cs
+++ Assets/Scripts/Playground/TimeCounter.cs
@@ -7,15 +7,31 @@ public class TimeCounter : MonoBehaviour {
 	public static float boringTime = 0;
 	private static float timeOver = 30;
 
+	static bool colorRed = false;
+
+
+	Color gray = Color.black * 0.3f;
+	Color red = Color.red * 0.7f;
 
 	// Use this for initialization
 	void Start () {
+		timer.color = gray;
 	}
 	
 	// Update is called once per frame
 	public void FixedUpdate2 () {
 		boringTime += Time.fixedDeltaTime;
 		timer.text = (timeOver - boringTime).ToString("00.00");
+		if(timeOver-boringTime < 3 && !colorRed){
+			if(!colorRed){
+				colorRed = true;
+				timer.color = red;
+			}
+		}
+		else if(colorRed){
+			colorRed = false;
+			timer.color = gray;
+		}
 		if (boringTime > timeOver) {
 			Match.DamageToAllShips (1);
 			TimeCounter.ReSetBoringTime (0.3f);
